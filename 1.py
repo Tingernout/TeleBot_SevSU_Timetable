95e8072ed25ce4a17a66685e224c4491609ea3d7
Tinger <roma.karpachev.84@mail.ru>
2024-07-16 20:34:46 +0300

    добавил алгоритм создания таблицы

--- 1.py
+++ 1.py
@@ -1,20 +1,26 @@
-import datetime
 from openpyxl import load_workbook
-file = load_workbook(filename='iit_23_b_o28.03.xlsx')
-day = datetime.date.today()
-today = day.strftime("%d.%m.%Y")
+file = load_workbook(filename='iit_23_b_o28.05.xlsx')
+data = [[[], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], []]]
+a = 23
+list = file['уч.н. ' + str(a)]
+stroca = 6
+dd = 7
+for data_day_element in range(6):
+    data[data_day_element].append(list['A' + str(dd)].value)
+    dd += 8
+    for data_para_number_element in range(0, 8):
+        stroca += 1
+        for stolbec in "CDEFG":
+            data[data_day_element][data_para_number_element].append(list[stolbec + str(stroca)].value)
 
-for a in range(23, 35):
-    list = file['уч.н. ' + str(a)]
-    for i in range(7, 56, 8):
-        if list['B' + str(i)].value == today:
-            for n in range(i, i + 8):
-                noneF = 'F'
-                noneG = 'G'
-                if list['F' + str(n)].value == None:
-                    noneF = "I"
-                    noneG = "J"
-                print(str(list['C' + str(n)].value) + " " + str(list['D' + str(n)].value) + " " + str(list['E' + str(n)].value) + " " + str(list[noneF + str(n)].value) + " " + str(list[noneG + str(n)].value) + "\n\n")
+
+
+
+
+
+print(data)
+
+
 
 
 
